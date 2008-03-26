82d71fb7081f60fda01323eee962fa99d269dc77
José Alburquerque <jaalburqu@svn.gnome.org>
2008-03-26 00:19:24 +0000

    Removed these because just realized that these are specific to a single
    2008-03-25  José Alburquerque  <jaalburqu@svn.gnome.org>
    
    	* gstreamer/src/generate-enums.sh:
    	* gstreamer/src/generate-methods.sh:
    	* gstreamerbase/src/generate-enums.sh:
    	* gstreamerbase/src/generate-methods.sh: Removed these because just
    	realized that these are specific to a single system
    
    svn path=/gstreamermm/trunk/; revision=1424

--- gstreamerbase/src/generate-enums.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Note that enum.pl should be in $PATH for this script to work
-
-PREFIX=/opt/usr-jhbuild
-
-enum.pl $PREFIX/include/gstreamer-0.10/gst/{audio,cdda,fft,floatcast,interfaces,netbuffer,riff,rtp,rtsp,sdp,tag,pbutils,video}/*.h > gstbase_enums.defs
--- gstreamerbase/src/generate-methods.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Note that h2def.py should be in $PATH for this script to work
-
-PREFIX=/opt/usr-jhbuild
-
-h2def.py $PREFIX/include/gstreamer-0.10/gst/{audio,cdda,fft,floatcast,interfaces,netbuffer,riff,rtp,rtsp,sdp,tag,pbutils,video}/*.h > gstbase_methods.defs
