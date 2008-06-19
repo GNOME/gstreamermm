e24d9c3c8206b8a2185fba342b584aa7b4462d6e
José Alburquerque <jaalburqu@svn.gnome.org>
2008-06-19 04:26:48 +0000

    Added simple pad test to confirm warning reported in bug #539063 which in
    2008-06-19  José Alburquerque  <jaalburqu@svn.gnome.org>
    
    	* tests/Makefile.am:
    	* tests/test-pad.cc: Added simple pad test to confirm warning reported
    	in bug #539063 which in fact does produce the warning. Filed gstreamer
    	bug #539055 explaining that some of their GObject instances are not
    	initialized with the construction properties.
    
    svn path=/gstreamermm/trunk/; revision=1568

---
+++ tests/test-pad.cc
@@ -0,0 +1,40 @@
+// -*- Mode: C++; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+
+/* gstreamermm - a C++ wrapper for gstreamer
+ *
+ * Copyright 2008 The gstreamermm Development Team
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the Free
+ * Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+#include <gstreamermm.h>
+#include <iostream>
+
+int main (int argc, char* argv[])
+{
+  Gst::init(argc, argv);
+
+  Glib::RefPtr<Gst::Pad> pad;
+
+  pad = Gst::Pad::create("test", Gst::PAD_SRC);
+
+  if (pad)
+  {
+    std::cout << "Successfully created pad '" << pad->get_name() << "'." <<
+      std::endl;
+  }
+
+  return 0;
+}
