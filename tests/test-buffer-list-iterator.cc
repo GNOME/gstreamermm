cd1b5877ca2fc41d7230c4c3ae88c25e7cf1472a
José Alburquerque <jaalburqu@svn.gnome.org>
2010-10-17 17:38:07 -0400

    Tests: BufferListIterator: Make sure the GType system is initialized.
    	* tests/test-buffer-list-iterator.cc: Initialize gstreamermm so that
    	the test executes successfully.  The code used for the last release
    	was slightly different and the new code seemed more correct, however
    	it was missing the initialization statement to make sure the GType
    	system was also initialized.

--- tests/test-buffer-list-iterator.cc
+++ tests/test-buffer-list-iterator.cc
@@ -1,7 +1,9 @@
 #include <gstreamermm.h>
 
-int main(int, char**)
+int main(int argc, char** argv)
 {
+  Gst::init(argc, argv);
+
   Glib::RefPtr<Gst::BufferList> list = Gst::BufferList::create();
   Gst::BufferListIterator test;
   list->iterate(test);
