0ee67ab7e89f8add1d53961bd623ac9ff9630fde
José Alburquerque <jaalburqu@svn.gnome.org>
2011-11-03 00:10:21 -0400

    Examples: Add a "Hello World" example.
    	* examples/Makefile.am:
    	* examples/hello_world/main.cc: Add a translation of the GStreamer
    	helloworld example so that the GStreamer bindings page can be updated
    	correctly (see the e-mail:
    	http://lists.freedesktop.org/archives/gstreamer-devel/2011-October/033776.html)

---
+++ examples/hello_world/main.cc
@@ -0,0 +1,114 @@
+/* gstreamermm - a C++ wrapper for gstreamer
+ *
+ * Copyright 2011 The gstreamermm Development Team
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
+#include <gstreamermm.h>
+#include <iostream>
+#include <stdlib.h>
+
+namespace
+{
+
+Glib::RefPtr<Glib::MainLoop> mainloop;
+
+// This function is used to receive asynchronous messages in the main loop.
+bool on_bus_message(const Glib::RefPtr<Gst::Bus>& /* bus */,
+  const Glib::RefPtr<Gst::Message>& message)
+{
+  switch(message->get_message_type()) {
+    case Gst::MESSAGE_EOS:
+      std::cout << std::endl << "End of stream" << std::endl;
+      mainloop->quit();
+      return false;
+    case Gst::MESSAGE_ERROR:
+    {
+      Glib::RefPtr<Gst::MessageError> msgError =
+        Glib::RefPtr<Gst::MessageError>::cast_dynamic(message);
+
+      if(msgError)
+      {
+        Glib::Error err;
+        err = msgError->parse();
+        std::cerr << "Error: " << err.what() << std::endl;
+      }
+      else
+        std::cerr << "Error." << std::endl;
+
+      mainloop->quit();
+      return false;
+    }
+    default:
+      break;
+  }
+
+  return true;
+}
+
+} // anonymous namespace
+
+int main(int argc, char** argv)
+{
+  // Initialize gstreamermm:
+  Gst::init(argc, argv);
+
+  // Check input arguments:
+  if(argc < 2)
+  {
+    std::cout << "Usage: " << argv[0] << " <media file or uri>" << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  // Create a playbin2 element.
+  Glib::RefPtr<Gst::PlayBin2> playbin = Gst::PlayBin2::create();
+
+  if(!playbin)
+  {
+    std::cerr << "The playbin2 element could not be created." << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  // Take the commandline argument and ensure that it is a uri:
+  Glib::ustring uri;
+
+  if(gst_uri_is_valid(argv[1]))
+    uri = argv[1];
+  else
+    uri = Glib::filename_to_uri(argv[1]);
+
+  // Set the playbyin2's uri property.
+  playbin->property_uri() = uri;
+
+  // Create the main loop.
+  mainloop = Glib::MainLoop::create();
+
+  // Get the bus from the playbin, and add a bus watch to the default main
+  // context with the default priority:
+  Glib::RefPtr<Gst::Bus> bus = playbin->get_bus();
+  bus->add_watch(sigc::ptr_fun(&on_bus_message));
+
+  // Now set the playbin to the PLAYING state and start the main loop:
+  std::cout << "Setting to PLAYING." << std::endl;
+  playbin->set_state(Gst::STATE_PLAYING);
+  std::cout << "Running." << std::endl;
+  mainloop->run();
+
+  // Clean up nicely:
+  std::cout << "Returned. Stopping playback." << std::endl;
+  playbin->set_state(Gst::STATE_NULL);
+
+  return EXIT_SUCCESS;
+}
