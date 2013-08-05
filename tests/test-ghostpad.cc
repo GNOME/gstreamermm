b295f55d9d4e1695fbf25915467608eb34528576
Marcin Kolny at Flytronic <marcin.kolny@flytronic.pl>
2013-08-05 08:48:57 +0200

    added ghostpad's tests

---
+++ tests/test-ghostpad.cc
@@ -0,0 +1,35 @@
+/*
+ * test-ghostpad.cc
+ *
+ *  Created on: Aug 5, 2013
+ *      Author: m.kolny
+ */
+
+#include <gtest/gtest.h>
+#include <gstreamermm.h>
+
+using namespace Gst;
+using Glib::RefPtr;
+
+TEST(GhostPadTest, CreatingGhostPad)
+{
+    Glib::ustring pad_name = "sample_name";
+    PadDirection direction = PAD_SINK;
+    RefPtr<GhostPad> ghost_pad = GhostPad::create(direction, pad_name);
+
+    ASSERT_TRUE(ghost_pad);
+    ASSERT_EQ(direction, ghost_pad->get_direction());
+    ASSERT_STREQ(pad_name.c_str(), ghost_pad->get_name().c_str());
+}
+
+TEST(GhostPadTest, CreatingGhostPadFromTemplate)
+{
+    PadDirection direction = PAD_SRC;
+    RefPtr<Caps> caps = Caps::create_simple("video/x-raw-yuv");
+    RefPtr<PadTemplate> templ = PadTemplate::create("pad-template", direction, PAD_ALWAYS, caps);
+
+    RefPtr<GhostPad> ghost_pad = GhostPad::create(templ);
+
+    ASSERT_TRUE(ghost_pad);
+    ASSERT_EQ(direction, ghost_pad->get_direction());
+}
