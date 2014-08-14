7ff7799f8b879db6a0e3f8b17215f513bcf31bcf
Marcin Kolny <marcin.kolny@gmail.com>
2014-08-14 13:42:47 +0200

    Gst::Pipeline: wrapped missing enums, added simple test
    	* gstreamer/src/pipeline.hg: wrapped PipelineFlags enum
    	* tests/Makefile.am:
    	* tests/test-pipeline.cc: added very simple test
    	* .gitignore: added pipeline output file to ignored list

---
+++ tests/test-pipeline.cc
@@ -0,0 +1,20 @@
+/*
+ * test-pipeline.cc
+ *
+ *  Created on: Aug 14, 2014
+ *      Author: m.kolny
+ */
+
+#include <gtest/gtest.h>
+#include <gstreamermm.h>
+
+using namespace Gst;
+
+TEST(PipelineTest, ShouldCorrectCreatePipelineWithSpecyficName)
+{
+  Glib::ustring name = "pipeline-name";
+  Glib::RefPtr<Pipeline> pipeline = Pipeline::create(name);
+
+  ASSERT_STREQ(name.c_str(), pipeline->get_name().c_str());
+  ASSERT_EQ(1, pipeline->get_refcount());
+}
