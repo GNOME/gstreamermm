9b835cd4b61f7d06deeffff1342c29dfeb6a80dd
Marcin Kolny <marcin.kolny@gmail.com>
2014-08-24 23:53:27 +0200

    Windows: added project files for Visual Studio 2010
    	* MSVC_2010/gstreamermm-1.0.props: property sheet (it should help
    	  gstreamermm's users create own project, which uses gstreamermm.
    	  File contains include directories, library paths needed for
    	  compilation apps based on gstreamermm.
    	* MSVC_2010/gstreamermm.sln: solution file
    	* MSVC_2010/gstreamermm/gstreamermm.rc: resources file
    	* MSVC_2010/gstreamermm/gstreamermm.vcxproj: gstreamermm visual
    	  studio's project file (also uses gstreamermm-1.0.props).
    	* MSVC_2010/gstreamermm/gstreamermm.vcxproj.filters: vs filters
    	  file.
    	* MSVC_2010/gstreamermm/gstreamermmconfig.h: file contains
    	  gstreamermm configuration.

---
+++ MSVC_2010/gstreamermm/gstreamermmconfig.h
@@ -0,0 +1,41 @@
+/* gstreamer/gstreamermmconfig.h.  Generated from gstreamermmconfig.h.in by configure.  */
+
+/* This is part of gstreamermm */
+
+#ifndef _GSTREAMERMM_CONFIG_H
+#define _GSTREAMERMM_CONFIG_H
+
+#include <glibmmconfig.h>
+
+/* Major version of gstreamermm */
+#define GSTREAMERMM_MAJOR_VERSION 1
+
+/* Minor version of gstreamermm */
+#define GSTREAMERMM_MINOR_VERSION 0
+
+/* Micro version of gstreamermm */
+#define GSTREAMERMM_MICRO_VERSION 10
+
+/* Define when building gstreamermm as a static library. */
+#define GSTREAMERMM_STATIC_LIB 1
+
+/* Enable DLL-specific stuff only when not building a static library. */
+#if (!defined(GSTREAMERMM_STATIC_LIB) && !defined(__CYGWIN__) && defined(_WIN32))
+# define GSTREAMERMM_DLL 1
+#endif
+
+#ifdef GSTREAMERMM_DLL
+# if defined(GSTREAMERMM_BUILD) && defined(_WINDLL)
+   /* Do not dllexport as it is handled by gendef on MSVC. */
+#  define GSTREAMERMM_API
+# elif !defined(GSTREAMERMM_BUILD)
+#  define GSTREAMERMM_API __declspec(dllimport)
+# else
+   /* Build a static library. */
+#  define GSTREAMERMM_API
+# endif /* GSTREAMERMM_BUILD - _WINDLL */
+#else
+# define GSTREAMERMM_API
+#endif /* GSTREAMERMM_DLL */
+
+#endif /* _GSTREAMERMM_CONFIG_H */
