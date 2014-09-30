df45fb6b3046a66c4c78aafd044a87e393bccc94
Marcin Kolny <marcin.kolny@gmail.com>
2014-10-01 01:30:07 +0200

    Gst::MiniObject: added a wrappers for the rest of nonexisting methods.
        * gstreamer/src/gst_extra_objects.defs: added extra class definition.
          for GstMiniObject struct.
        * gstreamer/src/miniobject.ccg:
        * gstreamer/src/miniobject.hg: added missing wrappers, also added
          QuarkData class, which should be used as a base class for data
          stored as a qdata.
        * tests/test-miniobject.cc: a few tests added, which check finalize
          notifier.

--- tests/test-miniobject.cc
+++ tests/test-miniobject.cc
@@ -13,24 +13,70 @@ using Glib::RefPtr;
 
 TEST(MiniObjectTest, ShouldCorrectCreateWritableObjects)
 {
-	// we cannot create Gst::MiniObject object, so I used Gst::Buffer::create
-	// method to make an object.
-	RefPtr<MiniObject> obj = Buffer::create();
+  // we cannot create Gst::MiniObject object, so I used Gst::Buffer::create
+  // method to make an object.
+  RefPtr<MiniObject> obj = Buffer::create();
 
-	ASSERT_EQ(1, obj->gobj()->refcount);
+  ASSERT_EQ(1, obj->gobj()->refcount);
 
-	obj = obj->create_writable();
+  obj = obj->create_writable();
 
-	ASSERT_EQ(1, obj->gobj()->refcount);
+  ASSERT_EQ(1, obj->gobj()->refcount);
 
-	RefPtr<MiniObject> obj2 = obj;
+  RefPtr<MiniObject> obj2 = obj;
 
-	ASSERT_EQ(2, obj->gobj()->refcount);
-	ASSERT_EQ(2, obj2->gobj()->refcount);
+  ASSERT_EQ(2, obj->gobj()->refcount);
+  ASSERT_EQ(2, obj2->gobj()->refcount);
 
-	obj2 = obj->create_writable();
+  obj2 = obj->create_writable();
 
-	ASSERT_EQ(1, obj->gobj()->refcount);
-	ASSERT_EQ(1, obj2->gobj()->refcount);
+  ASSERT_EQ(1, obj->gobj()->refcount);
+  ASSERT_EQ(1, obj2->gobj()->refcount);
 }
 
+TEST(MiniObjectTest, CheckFinalizeNotifier)
+{
+  int finalize_cnt = 0;
+  {
+    RefPtr<Buffer> obj = Buffer::create();
+    obj->add_finalize_notifier([&finalize_cnt](){
+      finalize_cnt++;
+    });
+  }
+
+  ASSERT_EQ(1, finalize_cnt);
+}
+
+TEST(MiniObjectTest, ShouldCorrectRemoveFinalizeHandler)
+{
+  int finalize_cnt = 0;
+  {
+    RefPtr<Buffer> obj = Buffer::create();
+    obj->add_finalize_notifier([&finalize_cnt](){
+      finalize_cnt++;
+    });
+    obj->remove_finalize_notifier();
+  }
+
+  ASSERT_EQ(0, finalize_cnt);
+}
+
+TEST(MiniObjectTest, AddRemoveFinalizerHandlerCombination)
+{
+  int finalize_cnt = 0;
+  {
+    RefPtr<Buffer> obj = Buffer::create();
+    obj->add_finalize_notifier([&finalize_cnt](){
+      finalize_cnt++;
+    });
+    obj->remove_finalize_notifier();
+    obj->add_finalize_notifier([&finalize_cnt](){
+      finalize_cnt += 17;
+    });
+    obj->add_finalize_notifier([&finalize_cnt](){
+      finalize_cnt += 104;
+    });
+  }
+
+  ASSERT_EQ(104, finalize_cnt);
+}
