cb123e9def0cca0e958718783c049b755b2337ba
Michał Wróbel <michal.wrobel@flytronic.pl>
2015-06-27 23:06:16 +0200

    Gst::BaseTransform: fix refs of {fixate,transform}_caps() return values
    This is https://bugzilla.gnome.org/show_bug.cgi?id=751601 in progress.
    
        * gstreamer/src/basetransform.hg: fix virtual methods transform_caps
          and fixate_caps.
        * tests/Makefile.am: add basetransform test to a build.
        * tests/plugins/derivedfrombasetransform.h:
        * tests/plugins/test-plugin-derivedfrombasetransform.cc: add
          basetransformunit tests which should fail if the fix done in
          gstreamer/src/basetransform.hg is not applied.

---
+++ tests/plugins/derivedfrombasetransform.h
@@ -0,0 +1,33 @@
+#ifndef TESTS_PLUGINS_DERIVEDFROMBASETRANSFORM_H_
+#define TESTS_PLUGINS_DERIVEDFROMBASETRANSFORM_H_
+
+#include <gstreamermm.h>
+#include <gstreamermm/private/basetransform_p.h>
+#include <assert.h>
+
+class DerivedFromBaseTransform : public Gst::BaseTransform
+{
+public:
+    static void base_init(Gst::ElementClass<DerivedFromBaseTransform> *klass)
+    {
+        klass->set_metadata("derivedfrombasetransform_longname",
+                "derivedfrombasetransform_classification", "derivedfrombasetransform_detail_description", "derivedfrombasetransform_detail_author");
+
+        klass->add_pad_template(Gst::PadTemplate::create("sink", Gst::PAD_SINK, Gst::PAD_ALWAYS, Gst::Caps::create_any()));
+        klass->add_pad_template(Gst::PadTemplate::create("src", Gst::PAD_SRC, Gst::PAD_ALWAYS, Gst::Caps::create_any()));
+    }
+
+    explicit DerivedFromBaseTransform(GstBaseTransform *gobj)
+        : Glib::ObjectBase(typeid (DerivedFromBaseTransform)), // type must be registered before use
+          Gst::BaseTransform(gobj)
+    {
+        set_passthrough(true);
+    }
+
+    static bool register_element(Glib::RefPtr<Gst::Plugin> plugin)
+    {
+        return Gst::ElementFactory::register_element(plugin, "derivedfrombasetransform", 10, Gst::register_mm_type<DerivedFromBaseTransform>("derivedfrombasetransform"));
+    }
+};
+
+#endif /* TESTS_PLUGINS_DERIVEDFROMBASETRANSFORM_H_ */
