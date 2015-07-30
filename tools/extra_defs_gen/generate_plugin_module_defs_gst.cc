dbf210114fd8862b177eb30217f5e053f51af661
Marcin Kolny <marcin.kolny@flytronic.pl>
2015-07-30 09:15:02 +0200

    extra_defs_gen: add application for generating plugins defs
        * .gitignore: add binary file to ignored list
        * gstreamer/src/gst_signals.defs:
        * gstreamer/src/gst_signals.defs.patch: regenerate defs files.
        * tools/Makefile.am: add new application to a build.
        * tools/extra_defs_gen/generate_defs_gst.cc: remove generating plugins
          definitions from generate_defs_gst application.
        * tools/extra_defs_gen/generate_plugin_module_defs_gst.cc: add source
          of application generating plugins definitions.
        * tools/extra_defs_gen/get_plugin_defs.{cc|h}: remove unused library.
        * tools/gen_scripts/generate_extra_defs.sh: update script generator.

---
+++ tools/extra_defs_gen/generate_plugin_module_defs_gst.cc
@@ -0,0 +1,68 @@
+/*
+ * generate_plugin_module_defs_gst.cc
+ *
+ *  Created on: Jul 30, 2015
+ *      Author: mkolny
+ */
+
+#include <glibmm_generate_extra_defs/generate_extra_defs.h>
+
+#include <gst/gst.h>
+
+#include <iostream>
+
+static std::string
+get_element_factory_defs(GstElementFactory *factory, GTypeIsAPointerFunc is_a_pointer_func)
+{
+  GType type = 0;
+  std::string result;
+
+  GstPluginFeature* loaded_feature = gst_plugin_feature_load(GST_PLUGIN_FEATURE(factory));
+  factory = GST_ELEMENT_FACTORY(loaded_feature);
+  type = gst_element_factory_get_element_type(factory);
+  result = get_defs(type, gtype_is_a_pointer);
+  g_object_unref(factory);
+  return result;
+}
+
+
+static gboolean
+feature_filter (GstPluginFeature *feature, gpointer user_data)
+{
+  GstPlugin *plugin = gst_plugin_feature_get_plugin (feature);
+  gchar *expected_source = reinterpret_cast<char*>(user_data);
+
+  if (plugin == NULL) {
+     return FALSE;
+  }
+
+  return g_strcmp0 (expected_source, gst_plugin_get_source (plugin)) == 0;
+}
+
+int main (int argc, char** argv)
+{
+  if (argc < 2) {
+    std::cerr << "Usage: " << argv[0] << " <source module>" << std::endl;
+    return -1;
+  }
+
+  gst_init (&argc, &argv);
+
+  GstRegistry *registry = gst_registry_get ();
+
+  GList *features_list = gst_registry_feature_filter (registry, feature_filter, FALSE, argv[1]);
+  GList *l = features_list;
+  while (l) {
+    GstPluginFeature *feature = reinterpret_cast<GstPluginFeature*>(l->data);
+
+    if (GST_IS_ELEMENT_FACTORY (feature)) {
+      std::cout << get_element_factory_defs (GST_ELEMENT_FACTORY_CAST (feature), gtype_is_a_pointer) << std::endl;
+    }
+    l = l->next;
+  }
+
+  gst_plugin_feature_list_free (features_list);
+
+
+
+}
--- tools/gen_scripts/generate_extra_defs.sh
+++ tools/gen_scripts/generate_extra_defs.sh
@@ -8,6 +8,8 @@ GEN_DIR="$ROOT_DIR/tools/extra_defs_gen"
 OUT_DIR="$ROOT_DIR/gstreamer/src"
 
 "$GEN_DIR"/generate_defs_gst > "$OUT_DIR"/gst_signals.defs
+"$GEN_DIR"/generate_plugin_module_defs_gst gstreamer >> "$OUT_DIR"/gst_signals.defs
+"$GEN_DIR"/generate_plugin_module_defs_gst gst-plugins-base >> "$OUT_DIR"/gst_signals.defs
 
 #Patch generated file.
 patch $OUT_DIR/gst_signals.defs $OUT_DIR/gst_signals.defs.patch
