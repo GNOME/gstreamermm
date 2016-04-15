1b3fb04a9e3cfe49a8e0f475b0a4046c19c17be9
Marcin Kolny <marcin.kolny@gmail.com>
2016-04-15 21:11:02 +0200

    Gst::Sample: increase reference in get_caps()
        * .gitignore: add generated test executable file to ignore list.
        * gstreamer/src/sample.{ccg|hg}: fix get_caps(), implement create().
        * tests/Makefile.am:
        * tests/test-sample.cc: add test for Gst::Sample class.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=765078

---
+++ tests/test-sample.cc
@@ -0,0 +1,23 @@
+#include <gtest/gtest.h>
+#include <gstreamermm.h>
+
+using namespace Gst;
+using Glib::RefPtr;
+
+TEST(SampleTest, CheckGetCapsMethod)
+{
+  auto caps = Caps::create_any();
+  ASSERT_EQ(1, caps->get_refcount());
+
+  auto sample = Sample::create(RefPtr<Buffer>(), caps, Segment(), Structure());
+  ASSERT_EQ(2, caps->get_refcount());
+
+  {
+    auto tmp_caps = sample->get_caps();
+    ASSERT_EQ(3, caps->get_refcount());
+    ASSERT_TRUE(tmp_caps->equals(caps));
+  }
+  
+  ASSERT_EQ(2, caps->get_refcount());
+
+}
